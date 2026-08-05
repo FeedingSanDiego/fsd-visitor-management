81164a90318319e713d0c021858d5470d1b4cde3
FeedingSanDiego <frontdesk@feedingsandiego.org>
2026-08-05 16:22:04 -0700

    Update cache name for service worker

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="fsd-visitor-v2";
+const CACHE="fsd-visitor-management-v1.1";
 const ASSETS=["./","./index.html","./manifest.json","./fsd-logo.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
